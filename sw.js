f2e7c617642e5ff30ee29f4aa4a3923118694f7e
mohankish24-gif <mohankish24@gmail.com>
2025-11-26 01:27:15 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
 const CACHE_NAME = "naam-jap-v1";
-const URLS_TO_CACHE = ["./", "./index.html"];
+const URLS = ["/Voice-naam-jap/", "/Voice-naam-jap/index.html"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
